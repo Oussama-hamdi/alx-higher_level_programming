9c8adb524b75fd41df4a376f6135f7adb6778781
root <ohamdi776@gmail.com>
2023-06-08 07:53:07 -0700

    a program that imports all functions from the file calculator_1.py and handles basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    arg_count = len(argv) - 1
+
+    if (arg_count != 3):
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    if argv[2] == "+":
+        result = add(int(argv[1]), int(argv[3]))
+
+    elif argv[2] == "-": 
+        result = sub(int(argv[1]), int(argv[3]))
+
+    elif argv[2] == "*":
+        result = mul(int(argv[1]), int(argv[3]))
+
+    elif argv[2] == "/":
+        result = div(int(argv[1]), int(argv[3]))
+
+    else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+    print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
