c01801a03a5ed24da5ee8abfcfa1a81e3cefe7d8
root <ohamdi776@gmail.com>
2023-06-11 07:50:26 -0700

    find the biggest integer of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        max = 0
+        for n in my_list:
+            if n > max:
+                max = n
+        return max
