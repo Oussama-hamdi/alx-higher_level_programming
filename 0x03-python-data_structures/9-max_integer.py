4a8d5f7720b811bd80cb34c8dd3d203d59a3070f
root <ohamdi776@gmail.com>
2023-06-11 08:00:51 -0700

    added parenthese to the return

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return (None)
     else:
         max_num = my_list[0]
         for n in my_list:
             if n > max_num:
                 max_num = n
-        return max_num
+    return (max_num)
