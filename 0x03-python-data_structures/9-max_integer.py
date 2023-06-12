1759036831145d58bd42de869bea32426e190a77
root <ohamdi776@gmail.com>
2023-06-12 02:55:56 -0700

    removed the else condition

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    else:
-        max_num = my_list[0]
-        for n in my_list:
-            if n > max_num:
-                max_num = n
+
+    max_num = my_list[0]
+    for n in my_list:
+        if n > max_num:
+            max_num = n
     return (max_num)
