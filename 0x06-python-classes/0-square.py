4887e4d44934f21d6d14d5a7965b3226c7ba6f73
root <ohamdi776@gmail.com>
2023-06-27 05:39:57 -0700

    fixed pycodestyle problem

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ A module defines a class Square """
 
+
 class Square:
     """ An empty class representing a square """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ A module defines a class Square """
 
+
 class Square:
     """ A class representing a square """
-
     def __init__(self, size):
         self.__size = size
     """ Class Init """
