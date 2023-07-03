0234b505d9b6b3d3b33a9c6975af0d2d79c8657d
root <ohamdi776@gmail.com>
2023-07-03 05:16:50 -0700

    fixed a code problem

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -38,7 +38,7 @@ class Rectangle:
     def height(self, val):
         """Setter method"""
         if val < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         if not isinstance(val, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         self.__height = val
