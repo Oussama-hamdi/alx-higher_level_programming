36596e41fa057d98ecb52f59cbc86dc643783669
root <ohamdi776@gmail.com>
2023-07-03 04:48:08 -0700

    added better documentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,6 +2,13 @@
 
 
 class Rectangle:
-    """Empty class that defines a rectangle"""
+    """Empty class that represents a rectangle
+
+    Attributes:
+        No attributes.
+
+    Methods:
+        No methods.
+    """
 
     pass
