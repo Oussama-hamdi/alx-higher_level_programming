3fdeb3bac498d3e035045c2ac981045bc5e4f19f
root <ohamdi776@gmail.com>
2023-07-11 05:17:14 -0700

    Completed task: 6. Create object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module provides a function to create
+    a Python object from a JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file"""
+    with open(filename) as file:
+        return json.load(file)
