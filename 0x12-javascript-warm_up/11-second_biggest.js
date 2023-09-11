400f4e6ca389ce9db7a412de145ed1bbce0f1446
root <ohamdi776@gmail.com>
2023-09-11 14:17:43 -0700

    Fixed wrong output bug

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,10 @@
 
 const args = process.argv.slice(2);
 
-if (args.length < 3) {
+if (args.length < 2) {
   console.log(0);
 } else {
   const newArr = args.map(Number);
   newArr.sort((a, b) => b - a);
-  console.log(newArr[1]);
+  console.log(newArr[1] || newArr[0]);
 }
