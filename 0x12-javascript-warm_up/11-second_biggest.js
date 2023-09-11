609df63f32cd77dfadf8791cc6c3439ea3cc93a3
root <ohamdi776@gmail.com>
2023-09-11 14:13:51 -0700

    Completed 11. Second biggest! task

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length <= 3) {
+  console.log(0);
+} else {
+  const newArr = args.map(Number);
+  newArr.sort((a, b) => b - a);
+  console.log(newArr[1]);
+}
