71aef4ba555734506eb635442e84c3b290986a74
Oussama-hamdi <ohamdi776@gmail.com>
2023-10-24 07:23:45 -0700

    Completed 5. Loripsum task

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* A script that gets the contents of a webpage and stores it in a file */
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url).pipe(fs.createWriteStream(filePath));
